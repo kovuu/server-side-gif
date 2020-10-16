2d0e7c083baf3f6d51e188274380a125291c05d4
kovuu <70741965+kovuu@users.noreply.github.com>
2020-10-16 16:02:50 +0300

    make it better

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "",
-    PASSWORD: "",
-    DB: "img_service",
+    HOST: "ec2-52-203-165-126.compute-1.amazonaws.com",
+    USER: "elxeyfgjsmvwtr",
+    PASSWORD: "4326d15131cd0c1421deb1c807f973f4451da1457b9b7ddb136913d308818761",
+    DB: "d16cq0n93r4bg6",
     dialect: "postgres",
     pool: {
         max: 5,
